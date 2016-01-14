9c196eb66a19b262f452ad886a01ab97ed33ab1f
Diana Falkowska <diana.falkowska@gmail.com>
2016-01-15 00:05:00 +0100

    add terminal text colors and ♡

--- git.sh
+++ git.sh
@@ -8,3 +8,11 @@ git config --global alias.co checkout
 git config --global alias.p-r 'pull --rebase'
 git config --global alias.p-ro '!b=$(git rev-parse --abbrev-ref HEAD);git pull --rebase origin "$b"'
 git config --global alias.shipit '!b=$(git rev-parse --abbrev-ref HEAD);git push origin "$b"'
+
+#colorful terminal
+cat > .bash_profile << EOF1
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="\[\033[1;35m\]\u ♡ \h\ \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+EOF1
