7e608b7ad3a5cd0b1af94d7f0b80cf7ac63389a1
dianafa <diana.falkowska@gmail.com>
2016-04-16 09:34:31 +0200

    master
    add alias for branch name in commit message

--- git.sh
+++ git.sh
@@ -5,6 +5,7 @@ git config --global alias.st status
 git config --global alias.uncam 'reset --soft HEAD~1'
 git config --global alias.cam 'commit -am'
 git config --global alias.co checkout
+git config --global alias.c '!b=$(git rev-parse --abbrev-ref HEAD);git commit -am "$b" -m'
 git config --global alias.p-r 'pull --rebase'
 git config --global alias.p-ro '!b=$(git rev-parse --abbrev-ref HEAD);git pull --rebase origin "$b"'
 git config --global alias.shipit '!b=$(git rev-parse --abbrev-ref HEAD);git push origin "$b"'
